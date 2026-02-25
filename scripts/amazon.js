eaa01542c6354b235eb4e092076b7e87585884e3
ShreyashMishra10 <shreyashmishra9044@gmail.com>
2026-02-25 14:40:41 +0530

    Made the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -52,11 +52,11 @@ products.forEach((product) => {
 })
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelectorAll("cart-event").forEach((button)=>{
+document.querySelectorAll(".cart-event").forEach((button)=>{
   button.addEventListener('click',()=>{
     const productId=button.dataset.productId;
 
-    let matchingItem=item; 
+    let matchingItem; 
     cart.forEach((item)=>{
       if(productId === item.productId){
         matchingItem=item
@@ -72,5 +72,12 @@ document.querySelectorAll("cart-event").forEach((button)=>{
       })
 
     }
+
+    let cartQuantity=0;
+    cart.forEach((item)=>{
+      cartQuantity+=item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML=cartQuantity;
   })
 })
